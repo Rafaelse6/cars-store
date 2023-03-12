01819cc94b2b68eba2ffc501808b9ad38babf4b7
Rafael Santos <rafa.05125@gmail.com>
2023-03-12 15:02:06 -0300

    Prettier

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,8 @@
-import "./assets/styles/custom.scss";
-import "./App.css";
+import './assets/styles/custom.scss';
+import './App.css';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return <h1>Hello World</h1>;
 }
 
 export default App;
