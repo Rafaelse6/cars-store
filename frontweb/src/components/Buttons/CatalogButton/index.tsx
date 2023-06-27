bf5777647e6e4ad643b60fe5cf2e74ad3447aeec
Rafael Santos <rafa.05125@gmail.com>
2023-06-27 09:13:40 -0300

    Buttons

---
+++ frontweb/src/components/Buttons/CatalogButton/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const CatalogButton = () => {
+  return (
+    <div className="btn-catalog-container ">
+      <button className="btn btn-catalog">
+        <span>VER CATÁLOGO</span>
+      </button>
+    </div>
+  );
+};
+
+export default CatalogButton;
